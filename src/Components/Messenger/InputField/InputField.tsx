2d8ddda8b986f9f11e93197e5d797d8e04fb75d1
Urban Vadzim <Herr.Wallace@protonmail.com>
2022-10-17 11:57:04 +0300

    feat: make SendButton for InputField component

--- src/Components/Messenger/InputField/InputField.tsx
+++ src/Components/Messenger/InputField/InputField.tsx
@@ -1,9 +1,13 @@
 import styles from './InputField.module.css';
+import { SendButton } from './SendButton';
 
 export const InputField = () => {
   return (
     <div className={styles.inputField}>
       <textarea className={styles.input}></textarea>
+      <button className={styles.sendButton}>
+        <SendButton />
+      </button>
     </div>
   );
 };
